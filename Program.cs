87bb72dcf44d23dce05ea26c223c57b5be52abe1
Василий <Wh1lepp@gmail.com>
2024-02-09 21:11:53 +0100

    added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// принимает на вход трехзначное число 
+// и удаляет вторую цифру этого числа.
+// Приимеры 
+// a = 256 => 26
+// b = 891 => 81
+
+int number = 563;
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+
+// $"Текст {result} текст {a - 100} текст {a + 400}"
+
+Console.WriteLine($"В числе {number} => {result}");
