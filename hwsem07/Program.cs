5798c6730ed274a6dfae50fa6d4b2b5acfb9b313
Василий <Wh1lepp@gmail.com>
2024-03-16 18:13:05 +0100

    end hw recursion

--- hwsem07/Program.cs
+++ hwsem07/Program.cs
@@ -4,19 +4,83 @@
 // использовать циклы.
 
 
-void ShowNumbers(int M, int N)
+// void ShowNumbers(int M, int N)
+// {
+//     if(M == N)
+//     {
+//         Console.Write(M);
+//         return;
+//     }
+//     Console.Write(M + " ");
+//     ShowNumbers(M + 1, N);
+// }
+
+// Console.Write("Введите число M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// ShowNumbers(M, N);
+
+
+// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
+
+// int Ackermann(int m, int n)
+// {
+//     if(m == 0) 
+//     {
+//         return n + 1;
+//     }
+//     else if(n == 0) 
+//     {
+//         return Ackermann(m - 1, 1);
+//     }
+//     else 
+//     {
+//         return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+// }
+
+// Console.Write(Ackermann(3, 2));
+
+
+// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+
+int[] CreateArray(int size)
 {
-    if(M == N)
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
     {
-        Console.Write(M);
-        return;
+        array[i] = new Random().Next(1, 101);
     }
-    Console.Write(M + " ");
-    ShowNumbers(M + 1, N);
+    // if(int i == array.Length)
+    // {
+
+    // }
+    return array;
 }
 
-Console.Write("Введите число M: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-ShowNumbers(1, 5);
+
+void PrintReverseArray(int[] array, int j)
+{
+    if(j == 0)
+    {
+        Console.Write(array[j]);
+        return;
+    } 
+        Console.Write(array[j] + "; ");
+        PrintReverseArray(array, j - 1);
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+Console.Write($"Результат: ");
+PrintReverseArray(array, size - 1);
--- hwsem07/obj/Debug/net8.0/hwsem07.AssemblyInfo.cs
+++ hwsem07/obj/Debug/net8.0/hwsem07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hwsem07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37365727af463939db80ac34d5ea68f7e3570a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a03f13f1df7f9777bf8469fca79f4c18e729c403")]
 [assembly: System.Reflection.AssemblyProductAttribute("hwsem07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hwsem07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
